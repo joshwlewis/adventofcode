2642d1ade7978ac315e69d3d7781be849079720f
Josh W Lewis <josh.w.lewis@gmail.com>
2019-12-12 14:20:02 -0600

    Add 2019 day 8 part 2

--- 2019.08/src/main.rs
+++ 2019.08/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self,Read};
+use std::io::{self, Read};
 use std::ops::RangeInclusive;
 
 fn main() {
@@ -7,14 +7,15 @@ fn main() {
 
     let image = Image::parse(imgstr, 25, 6);
 
-    let few0prod12 = findleast0sum12(image);
+    let few0prod12 = findleast0sum12(&image);
     println!("{}", few0prod12);
+
+    image.print();
 }
 
-
-fn findleast0sum12(image: Image) -> usize {
+fn findleast0sum12(image: &Image) -> usize {
     let mut layers = image.layers();
-    layers.sort_by(|la,lb| {
+    layers.sort_by(|la, lb| {
         let a0s = la.iter().filter(|p| p.c == 0).count();
         let b0s = lb.iter().filter(|p| p.c == 0).count();
         a0s.cmp(&b0s)
@@ -27,7 +28,7 @@ fn findleast0sum12(image: Image) -> usize {
     layer1s * layer2s
 }
 
-#[derive(Copy,Clone,Debug,Eq,PartialEq,Hash)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub struct Pixel {
     x: usize,
     y: usize,
@@ -35,34 +36,73 @@ pub struct Pixel {
     c: usize,
 }
 
-#[derive(Clone,Debug)]
+impl Pixel {
+    fn get(&self, axis: char) -> usize {
+        match axis {
+            'x' => self.x,
+            'y' => self.y,
+            'z' => self.z,
+            _ => panic!("only 3 dimensions!"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Image(Vec<Pixel>);
 
 impl Image {
     pub fn parse(s: String, xmax: usize, ymax: usize) -> Self {
         Self(
-            s.trim().chars().enumerate().map(|(i,c)| {
-                Pixel{
+            s.trim()
+                .chars()
+                .enumerate()
+                .map(|(i, c)| Pixel {
                     x: i % xmax,
                     y: (i / xmax) % ymax,
                     z: (i / (xmax * ymax)) % (xmax * ymax),
                     c: c.to_digit(10).unwrap() as usize,
-                }
-            }).collect()
+                })
+                .collect(),
         )
     }
 
-    fn zrange(&self) -> RangeInclusive<usize> {
-        let zs = self.0.iter().map(|p| p.z);
-        let zmin = zs.clone().min().unwrap();
-        let zmax = zs.max().unwrap();
-        zmin..=zmax
+    fn range(&self, axis: char) -> RangeInclusive<usize> {
+        let ps = self.0.iter().map(|p| p.get(axis));
+        let min = ps.clone().min().unwrap();
+        let max = ps.max().unwrap();
+        min..=max
+    }
+
+    fn color(&self, x: usize, y: usize) -> usize {
+        let mut pixels: Vec<&Pixel> = self.0.iter().filter(|p| x == p.x && y == p.y).collect();
+        pixels.sort_by(|pa, pb| pa.z.cmp(&pb.z));
+
+        let mut color = 2;
+        for p in pixels {
+            if color == 2 && p.c != 2 {
+                color = p.c
+            }
+        }
+        color
     }
 
     fn layers(&self) -> Vec<Vec<&Pixel>> {
-        self.zrange()
+        self.range('z')
             .map(|z| self.0.iter().filter(|p| p.z == z).collect())
             .collect()
+    }
+
+    fn print(&self) {
+        for y in self.range('y') {
+            for x in self.range('x') {
+                match self.color(x, y) {
+                    0 => print!("⬛️"),
+                    1 => print!("⬜️"),
+                    _ => print!("🔵"),
+                }
+            }
+            print!("\n");
+        }
     }
 }
 
@@ -73,10 +113,50 @@ mod tests {
     fn test_image_parse() {
         let imagestr = "012345678901234567890".to_string();
         let pixels = Image::parse(imagestr, 2, 3).0;
-        assert_eq!(pixels[0], Pixel{ x: 0, y: 0, z: 0, c: 0 });
-        assert_eq!(pixels[1], Pixel{ x: 1, y: 0, z: 0, c: 1 });
-        assert_eq!(pixels[3], Pixel{ x: 1, y: 1, z: 0, c: 3 });
-        assert_eq!(pixels[10], Pixel{ x: 0, y: 2, z: 1, c: 0 });
-        assert_eq!(pixels[19], Pixel{ x: 1, y: 0, z: 3, c: 9});
+        assert_eq!(
+            pixels[0],
+            Pixel {
+                x: 0,
+                y: 0,
+                z: 0,
+                c: 0
+            }
+        );
+        assert_eq!(
+            pixels[1],
+            Pixel {
+                x: 1,
+                y: 0,
+                z: 0,
+                c: 1
+            }
+        );
+        assert_eq!(
+            pixels[3],
+            Pixel {
+                x: 1,
+                y: 1,
+                z: 0,
+                c: 3
+            }
+        );
+        assert_eq!(
+            pixels[10],
+            Pixel {
+                x: 0,
+                y: 2,
+                z: 1,
+                c: 0
+            }
+        );
+        assert_eq!(
+            pixels[19],
+            Pixel {
+                x: 1,
+                y: 0,
+                z: 3,
+                c: 9
+            }
+        );
     }
 }
