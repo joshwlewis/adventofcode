1e86393524f6c06d4edf4bd920c66e06d4a85977
Josh W Lewis <josh.w.lewis@gmail.com>
2020-12-12 02:43:05 -0600

    Add 2020.12p2

--- 2020.12/main.go
+++ 2020.12/main.go
@@ -22,11 +22,16 @@ func main() {
 	is, err := ReadInstructions(file)
 	check(err)
 
-	f := Ferry{D: 90}
-	dist, err := f.Route(is)
+	d := Driver{D: 90}
+	dDist, err := Route(&d, is)
+	check(err)
+	fmt.Println("Driver Manhattan Distance: ", dDist)
+
+	c := Chaser{WX: 10, WY: 1}
+	cDist, err := Route(&c, is)
 	check(err)
 
-	fmt.Println("Manhattan Distance: ", dist)
+	fmt.Println("Chaser Manhattan Distance: ", cDist)
 }
 
 type Instruction struct {
@@ -35,10 +40,22 @@ type Instruction struct {
 }
 type Instructions []Instruction
 
-type Ferry struct {
+type Ferry interface {
+	Dist() int
+	Move(Instruction) error
+}
+
+type Driver struct {
 	X int
 	Y int
 	D int
+}
+
+type Chaser struct {
+	FX int
+	FY int
+    WX int
+	WY int
 }
 
 func ReadInstructions(r io.Reader) (Instructions, error) {
@@ -81,34 +98,70 @@ func (i Instruction) Change() (int, int, int, int, error) {
 	return 0, 0, 0, 0, fmt.Errorf("Invalid Action %c", i.Action)
 }
 
-func (f Ferry) Route(is Instructions) (int, error) {
+func Route(f Ferry, is Instructions) (int, error) {
 	for _, i := range is {
 		err := f.Move(i)
 		if err != nil {
 			return 0, err
 		}
 	}
-	return int(math.Abs(float64(f.X)) + math.Abs(float64(f.Y))), nil
+	return f.Dist(), nil
 }
 
-func (f *Ferry) Move(i Instruction) error {
+func (c Chaser) Dist() int {
+	return int(math.Abs(float64(c.FX)) + math.Abs(float64(c.FY)))
+}
+func (c *Chaser) Move(i Instruction) error {
 	dx, dy, dd, fw, err := i.Change()
 	if err != nil {
 		return err
 	}
-	if fw != 0 {
-		switch (f.D + 2880) % 360 {
-		case 0:
-			dy += fw
-		case 90:
-			dx += fw
-		case 180:
-			dy -= fw
-		case 270:
-			dx -= fw
-		default:
-			return fmt.Errorf("what angle is this?? %d, %d", f.D, f.D % 360)
-		}
+	c.WX, c.WY, err = Rotate(c.WX, c.WY, (dd+360) % 360)
+	if err != nil {
+		return err
+	}
+	c.WX+=dx
+	c.WY+=dy
+	for i := 0; i < fw; i++ {
+		c.FX+=c.WX
+		c.FY+=c.WY
+	}
+	return nil
+}
+
+func Rotate(x, y, ang int) (int, int, error) {
+	switch ang {
+	case 0:
+		return x, y, nil
+	case 90:
+		return y, -x, nil
+	case 180:
+		return -x, -y, nil
+	case 270:
+		return -y, x, nil
+	}
+	return 0, 0, fmt.Errorf("What angle is this?? %d", ang)
+}
+
+func (d Driver) Dist() int {
+	return int(math.Abs(float64(d.X)) + math.Abs(float64(d.Y)))
+}
+func (f *Driver) Move(i Instruction) error {
+	dx, dy, dd, fw, err := i.Change()
+	if err != nil {
+		return err
+	}
+	switch (f.D + 2880) % 360 {
+	case 0:
+		dy += fw
+	case 90:
+		dx += fw
+	case 180:
+		dy -= fw
+	case 270:
+		dx -= fw
+	default:
+		return fmt.Errorf("what angle is this?? %d, %d", f.D, f.D % 360)
 	}
 	f.X += dx
 	f.Y += dy
