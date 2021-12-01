98d1cf7e4b31d8d589210efec31362f3f8a24a8c
Josh W Lewis <josh.w.lewis@gmail.com>
2021-12-01 17:19:27 -0600

    Add 2020.19 WIP... does it work? :shrug:

---
+++ 2020.19/go.mod
@@ -0,0 +1,3 @@
+module github.com/joshwlewis/adventofcode/2020.19
+
+go 1.15
--- 2020.19/main.go
+++ 2020.19/main.go
@@ -23,25 +23,25 @@ func main() {
 
 	valMsgs := sys.CountMatching(Ref(0))
 	fmt.Println("Messages that match Rule 0", valMsgs)
-	check(err)
 }
 
 type Message string
 type Letter rune
 type Ref int
-type Series [2]Ref
+type Series [2]Rule
 type Option [2]Rule
 type Rule interface {
 	Messages(*System) map[Message]bool
 }
 type System struct {
 	Messages []Message
-	Rules map[Ref]Rule
-	Cache map[Ref]map[Message]bool
+	Rules    map[Ref]Rule
+	Cache    map[Ref]map[Message]bool
 }
 
 func (sys System) CountMatching(r Ref) (sum int) {
 	valMsgs := sys.Rules[r].Messages(&sys)
+	fmt.Println("valmsgs", len(valMsgs))
 	for _, msg := range sys.Messages {
 		if val, ok := valMsgs[msg]; val && ok {
 			sum++
@@ -50,13 +50,13 @@ func (sys System) CountMatching(r Ref) (sum int) {
 	return
 }
 
-func (l Letter) Messages(sys *System) (map[Message]bool) {
+func (l Letter) Messages(sys *System) map[Message]bool {
 	msgs := make(map[Message]bool)
 	msgs[Message(string(l))] = true
 	return msgs
 }
 
-func (s Series) Messages(sys *System) (map[Message]bool) {
+func (s Series) Messages(sys *System) map[Message]bool {
 	msgs := make(map[Message]bool)
 	for msgA := range s[0].Messages(sys) {
 		for msgB := range s[1].Messages(sys) {
@@ -66,16 +66,31 @@ func (s Series) Messages(sys *System) (map[Message]bool) {
 	return msgs
 }
 
-func (r Ref) Messages(sys *System) (map[Message]bool) {
+func (r Ref) Messages(sys *System) map[Message]bool {
+	var ms map[Message]bool
 	if ms, ok := sys.Cache[r]; ok {
 		return ms
 	}
-	ms := sys.Rules[r].Messages(sys)
-	sys.Cache[r] = ms
+	fmt.Println("Looking up R", r)
+	os.Chmod("foo", os.FileMode(4))
+	switch {
+	case r == Ref(8):
+		ms = Ref(-2).Messages(sys)
+	case r == Ref(11):
+		ms = Ref(-1).Messages(sys)
+	case r < -6:
+		ms = map[Message]bool{Message("x"): true}
+	case r < 0 && (r%2 == 0):
+		ms = Option{Ref(42), Series{Ref(42), Ref(r - 2)}}.Messages(sys)
+	case r < 0:
+		ms = Option{Series{Ref(42), Ref(31)}, Series{Series{Ref(42), Ref(r - 2)}, Ref(31)}}.Messages(sys)
+	default:
+		ms = sys.Rules[r].Messages(sys)
+	}
 	return ms
 }
 
-func (o Option) Messages(sys *System) (map[Message]bool) {
+func (o Option) Messages(sys *System) map[Message]bool {
 	msgs := make(map[Message]bool)
 	for _, s := range o {
 		for m := range s.Messages(sys) {
