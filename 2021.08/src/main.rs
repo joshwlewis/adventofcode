9b31aca9e44d039da139e4e72c5d90798196cb8a
Josh W Lewis <josh.lewis@salesforce.com>
2021-12-08 13:46:31 -0600

    Refactor digit data type for 2021.08

--- 2021.08/src/main.rs
+++ 2021.08/src/main.rs
@@ -2,7 +2,6 @@ use std::str::FromStr;
 use std::num::ParseIntError;
 use std::env;
 use std::fs;
-use std::fmt;
 
 fn main() {
     let filename = match env:: args().nth(1) {
@@ -42,16 +41,9 @@ struct Segment {
     outputs: [Digit; 4],
 }
 
-#[derive(PartialEq, Eq, Hash, Default, Copy, Clone)]
-struct Digit {
-    a: bool,
-    b: bool,
-    c: bool,
-    d: bool,
-    e: bool,
-    f: bool,
-    g: bool,
-}
+#[derive(PartialEq, Eq, Hash, Default, Debug, Clone)]
+struct Digit { s: String }
+const DEFAULT_DIGIT: Digit = Digit{s: String::new()};
 
 impl FromStr for Segment {
     type Err = ParseIntError;
@@ -61,7 +53,7 @@ impl FromStr for Segment {
             Some(s) => s,
             None => "",
         };
-        let mut signals: [Digit; 10] = [Default::default(); 10];
+        let mut signals: [Digit; 10] = [DEFAULT_DIGIT; 10];
         for (i, s) in signals_str.split(' ').enumerate() {
             let digit = s.parse::<Digit>()?;
             signals[i] = digit;
@@ -70,26 +62,17 @@ impl FromStr for Segment {
             Some(s) => s,
             None => "",
         };
-        let mut outputs: [Digit; 4] = [Default::default(); 4];
+        let mut outputs: [Digit; 4] = [DEFAULT_DIGIT; 4];
         for (i,s) in digits_str.split(' ').enumerate() {
             let digit = s.parse::<Digit>()?;
             outputs[i] = digit;
         }
         return Ok(Segment { signals, outputs })
     }
-
 }
 impl Digit {
     fn count(&self) -> usize {
-        let mut count = 0;
-        if self.a { count+=1 };
-        if self.b { count+=1 };
-        if self.c { count+=1 };
-        if self.d { count+=1 };
-        if self.e { count+=1 };
-        if self.f { count+=1 };
-        if self.g { count+=1 };
-        return count;
+        self.s.chars().count()
     }
 }
 
@@ -97,47 +80,9 @@ impl FromStr for Digit {
     type Err = ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // Examples: "cb", "fdcagb"
-        let mut d = Digit{a:false,b:false,c:false,d:false,e:false,f:false,g:false};
-        for c in s.chars() {
-            match c {
-                'a' => d.a = true,
-                'b' => d.b = true,
-                'c' => d.c = true,
-                'd' => d.d = true,
-                'e' => d.e = true,
-                'f' => d.f = true,
-                'g' => d.g = true,
-                _ => (),
-            }
-        }
-        return Ok(d);
-    }
-}
-
-impl fmt::Debug for Digit {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = String::from("");
-        if self.a {
-            s.push('a');
-        }
-        if self.b {
-            s.push('b');
-        }
-        if self.c {
-            s.push('c');
-        }
-        if self.d {
-            s.push('d');
-        }
-        if self.e {
-            s.push('e');
-        }
-        if self.f {
-            s.push('f')
-        }
-        if self.g {
-            s.push('g');
-        }
-        write!(f, "Digit({})", s)
+        let mut chars = s.chars().collect::<Vec<char>>();
+        chars.sort();
+        let ds: String = chars.iter().collect();
+        return Ok(Digit{ s: ds })
     }
 }
