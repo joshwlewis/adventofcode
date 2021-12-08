900eb4bdbc9fbbd67ff113d43317659ac8b2a8ff
Josh W Lewis <josh.lewis@salesforce.com>
2021-12-08 17:24:02 -0600

    Add 2020.08 part 2

--- 2021.08/src/main.rs
+++ 2021.08/src/main.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 use std::num::ParseIntError;
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let filename = match env:: args().nth(1) {
@@ -10,8 +11,10 @@ fn main() {
     };
     let input = fs::read_to_string(filename).expect("Error opening input file");
     let segment_data = parse_segment_data(input).unwrap();
-    let count_1478 = count_1478_output(segment_data);
+    let count_1478 = count_1478_output(&segment_data);
     println!("1478 Output Count: {}", count_1478);
+    let sum = sum_outputs(segment_data);
+    println!("Output Sum: {}", sum);
 }
 
 fn parse_segment_data(input: String) -> Result<SegmentData, ParseIntError> {
@@ -26,12 +29,69 @@ fn parse_segment_data(input: String) -> Result<SegmentData, ParseIntError> {
     return Ok(seg_data);
 }
 
-fn count_1478_output(segments: SegmentData) -> usize {
+fn count_1478_output(segments: &SegmentData) -> usize {
     segments.iter().map(|seg| seg.outputs.iter().filter(|d| {
             let c = d.count();
             return c == 2 || c == 3 || c == 4 || c == 7;
     }).count()).sum()
 }
+
+fn sum_outputs(segments: SegmentData) -> usize {
+    let ten: usize = 10;
+    segments.iter().map(|seg| {
+        let mut sum: usize = 0;
+        let digit_map = get_digit_map(&seg.signals);
+        let mut outputs = seg.outputs.clone();
+        outputs.reverse();
+        for (i, d) in outputs.iter().enumerate() {
+            match digit_map.get(&d) {
+                Some(val) => { sum += ten.pow(i as u32) * val; },
+                None => { panic!("didn't find {:?} in key {:?} for signal {:?}", d, digit_map, seg.signals) },
+            }
+        }
+        return sum;
+    }).sum()
+}
+
+fn get_digit_map(signals: &[Digit; 10]) -> HashMap<Digit, usize> {
+    let mut digit_map: HashMap<Digit, usize> = HashMap::new();
+
+    let mut four = DEFAULT_DIGIT;
+    let mut seven = DEFAULT_DIGIT;
+    for d in signals {
+        match d.count() {
+            2 => { digit_map.insert(d.clone(), 1); },
+            4 => {
+                four = d.clone();
+                digit_map.insert(d.clone(), 4);
+            }
+            3 => {
+                seven = d.clone();
+                digit_map.insert(d.clone(), 7);
+            },
+            7 => { digit_map.insert(d.clone(), 8); }
+            _ => (),
+        };
+    }
+    for d in signals {
+        let val = match (d.count(), d.missing_from(&four), d.missing_from(&seven)) {
+            (6, 1, 0) => 0,
+            (2, _, _) => 1,
+            (5, 2, 1) => 2,
+            (5, 1, 0) => 3,
+            (4, _, _) => 4,
+            (5, 1, 1) => 5,
+            (6, 1, 1) => 6,
+            (3, _, _) => 7,
+            (7, _, _) => 8,
+            (6, 0, 0) => 9,
+            t => panic!("don't know what to do with {:?} in {:?} -- {:?}", d, signals, t),
+        };
+        digit_map.insert(d.clone(), val);
+    }
+    return digit_map;
+}
+
 
 type SegmentData = Vec<Segment>;
 
@@ -74,13 +134,25 @@ impl Digit {
     fn count(&self) -> usize {
         self.s.chars().count()
     }
+
+    fn missing_from(&self, other: &Self) -> usize {
+        let mut missing: usize = 0;
+        for oc in other.s.chars() {
+            if !self.s.contains(oc) {
+                missing += 1
+            }
+        }
+        return missing;
+    }
 }
 
 impl FromStr for Digit {
     type Err = ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // Examples: "cb", "fdcagb"
-        let mut chars = s.chars().collect::<Vec<char>>();
+        let mut chars = s.chars()
+            .filter(|c| c.is_alphabetic() && c.is_lowercase())
+            .collect::<Vec<char>>();
         chars.sort();
         let ds: String = chars.iter().collect();
         return Ok(Digit{ s: ds })
