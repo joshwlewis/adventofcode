5bc9d18c1eb0738420792b37abcac1f37ff30859
Josh W Lewis <josh.lewis@salesforce.com>
2021-12-09 09:54:52 -0600

    Add 2021.09 part 2

--- 2021.09/src/main.rs
+++ 2021.09/src/main.rs
@@ -2,6 +2,8 @@ use std::str::FromStr;
 use std::num::ParseIntError;
 use std::env;
 use std::fs;
+use std::collections::VecDeque;
+use std::collections::HashSet;
 
 fn main() {
     let filename = match env:: args().nth(1) {
@@ -14,10 +16,20 @@ fn main() {
     let lows = floor.find_lows();
     let risk_sum: usize = risk_sum(lows.clone());
     println!("Lows: {:?}, Risk Sum: {}", lows, risk_sum);
+    let basins = floor.find_basins(lows);
+    let basin_sum = largest_basin_prod(basins.clone());
+    println!("Basins: {:?}, Size Prod: {}", basins, basin_sum);
 }
 
-fn risk_sum(depths: Vec<usize>) -> usize {
-    depths.iter().sum::<usize>() + depths.iter().count()
+fn risk_sum(lows: Vec<(usize, usize)>) -> usize {
+    let depths = lows.iter().map(|l| l.1);
+    depths.clone().sum::<usize>() + depths.count()
+}
+
+fn largest_basin_prod(basins: Vec<usize>) -> usize {
+    let mut bs = basins;
+    bs.sort_unstable();
+    bs[bs.len()-3..bs.len()].iter().product()
 }
 
 #[derive(Debug,Clone)]
@@ -27,34 +39,64 @@ struct Floor {
 }
 
 impl Floor {
-    fn find_lows(&self) -> Vec<usize> {
+    fn find_lows(&self) -> Vec<(usize, usize)> {
         let mut lows = vec![];
         'd: for (i, depth) in self.depths.iter().enumerate() {
-            for adj in self.get_adjacent_depths(i) {
-                if adj <= *depth {
+            for j in self.get_adjacent_indices(i) {
+                if self.depths[j] <= *depth {
                     continue 'd;
                 }
             }
-            lows.push(*depth);
+            lows.push((i,*depth));
         }
         lows
     }
 
-    fn get_adjacent_depths(&self, i: usize) -> Vec<usize> {
-        let mut adj_depths = vec![];
+    fn find_basins(&self, lows: Vec<(usize, usize)>) -> Vec<usize> {
+        let mut basins = vec![];
+        for (i, _) in lows {
+            let mut size = 1;
+            let mut queue: VecDeque<usize> = VecDeque::new();
+            let mut checked = HashSet::from([i]);
+            for j in self.get_adjacent_indices(i) {
+                queue.push_back(j);
+            }
+            loop {
+                let j: usize;
+                match queue.pop_front() {
+                    Some(n) => { j = n; },
+                    None => { break; },
+                }
+                if checked.get(&j).is_some() { continue; }
+                if self.depths[j] == 9 {
+                    continue;
+                }
+                size += 1;
+                checked.insert(j);
+                for k in self.get_adjacent_indices(j) {
+                    queue.push_back(k);
+                }
+            }
+            basins.push(size);
+        }
+        basins
+    }
+
+    fn get_adjacent_indices(&self, i: usize) -> Vec<usize> {
+        let mut adj_idxs = vec![];
         if i % self.width != 0 {
-            adj_depths.push(self.depths[i-1]);
+            adj_idxs.push(i-1);
         }
         if i % self.width != self.width - 1 {
-            adj_depths.push(self.depths[i+1]);
+            adj_idxs.push(i+1);
         }
         if i / self.width != 0 {
-            adj_depths.push(self.depths[i-self.width])
+            adj_idxs.push(i-self.width)
         }
         if i / self.width < (self.depths.len() / self.width)-1 {
-            adj_depths.push(self.depths[i+self.width])
+            adj_idxs.push(i+self.width)
         }
-        adj_depths
+        adj_idxs
     }
 }
 impl FromStr for Floor{
@@ -70,6 +112,6 @@ impl FromStr for Floor{
             }
         }
 
-        Ok(Floor{width: width, depths: depths})
+        Ok(Floor{width, depths})
     }
 }
